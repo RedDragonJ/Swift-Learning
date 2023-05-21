9a00e1d748e2cb35441a8e7a6ac85cbce10854dc
James Layton <>
2023-05-20 22:37:09 -0400

    Firebase Integration with SwiftUI in iOS App: Part 1

---
+++ LearnFirebaseInSwiftUI/LearnFirebaseInSwiftUI/LearnFirebaseInSwiftUIApp.swift
@@ -0,0 +1,31 @@
+//
+//  LearnFirebaseInSwiftUIApp.swift
+//  LearnFirebaseInSwiftUI
+//
+//  Created by James Layton on 5/20/23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+
+@main
+struct LearnFirebaseInSwiftUIApp: App {
+    
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
