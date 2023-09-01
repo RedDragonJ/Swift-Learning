19e058e051d7e8e6a7c4fd8eab406e7d1cda41ce
James Layton <>
2023-09-01 00:38:42 -0400

    - MetaMask part 1

--- SwiftUIAndMetaMask/MetaSwiftUI/MetaSwiftUIApp.swift
+++ SwiftUIAndMetaMask/MetaSwiftUI/MetaSwiftUIApp.swift
@@ -7,8 +7,19 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        // Handle the URL here
+        print(url)
+        return true
+    }
+}
+
 @main
 struct MetaSwiftUIApp: App {
+    
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
